d0a4555b1b591714ea7724a0c0c0ef3081c6cb8a
JaniceBetty <janicebetty5@gmail.com>
2026-03-01 02:48:30 +0530

    fix: resolve route handling issue

--- flask_blog/users/routes.py
+++ flask_blog/users/routes.py
@@ -32,7 +32,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('main.login'))
+        return redirect(url_for('users.login'))
     return render_template('register.html', title= 'Register', form=form)
 
 @users.route("/login", methods=['GET', 'POST'])
